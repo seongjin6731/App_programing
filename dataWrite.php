197ccb65eb717106921204a338da8e6c3a4f9e75
seongjin6731 <gkstjdwls147@naver.com>
2023-04-18 22:48:39 +0900

    DB연결
    MariaDB 연동 후 html에서 입력받은 데이터값을 생성한 database인 test에 입력하는 구문

---
+++ dataWrite.php
@@ -0,0 +1,43 @@
+<html>
+<!-- <meta charset="utf-8"> -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<?php
+
+	$host = 'localhost';
+	$user = 'admin';
+	$pw = '1234';
+	$dbName = 'test';
+	$mysqli = new mysqli($host, $user, $pw, $dbName);
+
+	$data_title = $_POST['title'];
+	$data_title = addslashes($data_title);
+    $data_data = $_POST['data'];
+    $data_data = addslashes($data_data);
+    
+
+	$sql = "insert into data (
+			data_title,
+			data_data
+	)";
+	
+	$sql = $sql. "values (
+			'$data_title',
+			'$data_data'
+	)";
+
+	if($mysqli->query($sql)){ 
+	  echo '<script>alert("success inserting")</script>'; 
+	}else{ 
+	  echo '<script>alert("fail to insert sql")</script>';
+	}
+
+	mysqli_close($mysqli);
+  
+?>
+
+<script>
+	location.href = "data_input.html";
+</script>
+
+</html>
